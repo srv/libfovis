8c1fc65de4423547116a99e2264bc08a0e4d53b1
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-10-01 18:47:20 +0100

    Refactoring libfovis to comply with ROS standards and creating suitable install targets
    See #5

---
+++ include/libfovis/fovis.hpp
@@ -0,0 +1,36 @@
+#ifndef __fovis_fovis_hpp__
+#define __fovis_fovis_hpp__
+
+#include "libfovis/visual_odometry.hpp"
+
+#include "libfovis/primesense_depth.hpp"
+#include "libfovis/stereo_depth.hpp"
+#include "libfovis/depth_image.hpp"
+
+/**
+ * \defgroup FovisCore Fovis core
+ * \brief Core data structures and algorithms
+ */
+
+/**
+ * \defgroup DepthSources Depth sources
+ * \brief Classes for specific depth sources (e.g., stereo, Kinect)
+ */
+
+/**
+ * Fovis major version number.  Use this to check which version of fovis you
+ * have.
+ */
+#define FOVIS_MAJOR_VERSION 1
+
+/**
+ * Fovis minor version number.
+ */
+#define FOVIS_MINOR_VERSION 0
+
+/**
+ * Fovis micro version number.
+ */
+#define FOVIS_MICRO_VERISON 0
+
+#endif
